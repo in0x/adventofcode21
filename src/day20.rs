1d1e8cfb389671e430d77a0f667960a759dd7672
Philipp Welsch <pwelsch86@gmail.com>
2021-12-20 14:10:35 +0000

    d20p1

--- src/day20.rs
+++ src/day20.rs
@@ -1,42 +1,6 @@
 use super::common;
 use std::path::Path;
 
-/// Same as get_box_taps, but the taps are in ascending row order:
-/// x+ __
-/// +y|[0 1 2]
-///    [3 4 5]
-///    [6 7 8]
-pub fn get_box_taps_row_order(idx: usize, width: usize, height: usize) -> [Option<usize>; 9] {
-    let (x, y) = {
-        let (x, y) = common::get_grid_xy(idx, width, height);
-        (x as i32, y as i32)
-    };
-
-    let mut coords: [Option<usize>; 9] = Default::default();
-    let mut coord_i = 0;
-
-    for d_y in [-1,0,1] {
-        let next_y = y + d_y;
-        if next_y < 0 || next_y >= height as i32 {
-            continue;
-        }
-    
-        for d_x in [-1,0,1] {
-            let next_x = x + d_x;
-            if next_x < 0 || next_x >= width as i32 {
-                continue;
-            }
-
-            let tap = next_x as usize + (next_y as usize * width);
-            coords[coord_i] = Some(tap);
-            coord_i += 1;
-        }
-    }
-    
-    
-    coords
-}
-
 fn print_image(img: &[char], width: usize) {
     for i in 0..img.len() {
         if (i % width) == 0 {
@@ -45,6 +9,68 @@ fn print_image(img: &[char], width: usize) {
         print!("{}", img[i]);
     }
     print!("\n");
+}
+
+fn lookup_str_to_number(lookup_str: &[char]) -> u32 {
+    assert_eq!(9, lookup_str.len());
+    let mut output = 0;
+    for i in 0..lookup_str.len() {
+        if lookup_str[i] == '#' {
+            output |= 1 << (8 - i);
+        } else {
+            assert_eq!('.', lookup_str[i]);
+        }
+    }
+    output
+}
+
+/// Returns a triple of (new image, new width, new height)
+fn enhance_image(default_pixel: char, image: &[char], width: usize, height: usize, enhancer: &[char]) -> (Vec<char>, usize, usize) {
+    let new_image_width = width + 2; // Add row top and bottom
+    let new_image_height = height + 2; // Add col left and right
+    let mut new_image = Vec::new();
+    new_image.resize(new_image_width * new_image_height, '.');
+
+    for col in 0..(new_image_height as i32) {
+        for row in 0..(new_image_width as i32) {
+            let src_row = row - 1;
+            let src_col = col - 1;
+
+            let mut lookup_string = Vec::new();
+
+            // Tap the image in descending row order
+            for j in [-1,0,1] {
+                let y_tap = src_col + j;
+                
+                for i in [-1,0,1] {
+                    let x_tap = src_row + i;
+                    
+                    if y_tap < 0 || y_tap >= (height as i32) {
+                        lookup_string.push(default_pixel);
+                        continue;
+                    } else if x_tap < 0 || x_tap >= (width as i32) {
+                        lookup_string.push(default_pixel);
+                        continue;
+                    }
+
+                    let tap = x_tap as usize + (y_tap as usize * width);
+                    lookup_string.push(image[tap]);
+                }
+            }
+                
+            let lookup_idx = lookup_str_to_number(&lookup_string);
+            let new_image_idx = row as usize + (col as usize * new_image_width);
+            new_image[new_image_idx] = enhancer[lookup_idx as usize];
+        }
+    }
+
+
+    print_image(&image, width);
+    println!("====== Before ======");
+    print_image(&new_image, new_image_width);
+    println!("====== After ======");
+
+    (new_image, new_image_width, new_image_height)
 }
 
 pub fn run(root_dir: &Path) {
@@ -57,7 +83,7 @@ pub fn run(root_dir: &Path) {
             .collect::<Vec<_>>()
     };
 
-    let (image, width) = {
+    let (mut image, mut width) = {
         let mut skip_to = 0;
         while !common::is_newline(bytes[skip_to] as char) {
             skip_to += 1;
@@ -75,63 +101,16 @@ pub fn run(root_dir: &Path) {
         (image, width)
     };
 
-    let height = image.len() / width;
-
-    let new_image_width = width + 2; // Add row top and bottom
-    let new_image_height = height + 2; // Add col left and right
-    let mut new_image = Vec::new();
-    new_image.resize(new_image_width * new_image_height, '.');
-
-
-    fn lookup_str_to_number(lookup_str: &[char]) -> u32 {
-        assert_eq!(9, lookup_str.len());
-        let mut output = 0;
-        for i in 0..lookup_str.len() {
-            if lookup_str[i] == '#' {
-                output |= 1 << (8 - i);
-            } else {
-                assert_eq!('.', lookup_str[i]);
-            }
-        }
-        output
+    let mut height = image.len() / width;
+    let iterations = 2;
+    for gen in 0..iterations {
+        let default_pixel = if gen % 2 == 0 { '.' } else { '#' };
+        let (new_image, new_width, new_height) = enhance_image(default_pixel, &image, width, height, &enhancement);
+        image = new_image;
+        width = new_width;
+        height = new_height;
     }
 
-    for col in 0..(new_image_height as i32) {
-        for row in 0..(new_image_width as i32) {
-            let src_row = row - 1;
-            let src_col = col - 1;
-
-            let mut lookup_string = Vec::new();
-
-            // Tap the image in descending row order
-            for j in [-1,0,1] {
-                let y_tap = src_col + j;
-                
-                for i in [-1,0,1] {
-                    let x_tap = src_row + i;
-                    
-                    if y_tap < 0 || y_tap >= (height as i32) {
-                        lookup_string.push('.');
-                        continue;
-                    }
-                    else if x_tap < 0 || x_tap >= (width as i32) {
-                        lookup_string.push('.');
-                        continue;
-                    }
-
-                    let tap = x_tap as usize + (y_tap as usize * width);
-                    lookup_string.push(image[tap]);
-                }
-            }
-              
-            let lookup_idx = lookup_str_to_number(&lookup_string);
-            let new_image_idx = row as usize + (col as usize * new_image_width);
-            new_image[new_image_idx] = enhancement[lookup_idx as usize];
-        }
-    }
-
-    println!("Before:");
-    print_image(&image, width);
-    println!("After:");
-    print_image(&new_image, new_image_width);
+    let lit_pixels = image.iter().filter(|c| **c == '#').count();
+    println!("Num lit pixels {}", lit_pixels);
 }
